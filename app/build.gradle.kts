9fdedcfac639ee9caf82b94215e0bee2604f0ce6
usuariozombie <edupime_98@hotmail.com>
2024-01-07 21:12:05 +0100

    package name edited

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.vedruna.pimentelgilmunillam02"
+    namespace = "com.vedruna.pimentelgilmunillam01"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.vedruna.pimentelgilmunillam02"
+        applicationId = "com.vedruna.pimentelgilmunillam01"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "PimentelGilMunillaM02"
+rootProject.name = "PimentelGilMunillaM01"
 include(":app")
  
